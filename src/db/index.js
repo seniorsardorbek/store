22301ba07d2af97e2f840ba49c5ee3e69051678b
Sardorbek <sardorbekmusilman@gmail.com>
2025-01-23 19:51:41 +0500

    heyy

--- src/db/index.js
+++ src/db/index.js
@@ -5,10 +5,10 @@ import config from "../utils/config/config.js"
 export async function db() {
     console.log(config.db.database);
     return mongoose
-        // .connect(`mongodb+srv://${config.db.username}:${config.db.password}@cluster0.9dygz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster/${config.db.database}`, {
+        // .connect(`mongodb//27017:${config.db.database}`, {
         .connect(`mongodb://localhost:27017/${config.db.database}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         })
         .then((res) => console.log('DB connected succesfully'))
         .catch((err) => console.log('Error connecting'));
