ba578baef0ccb7ec0d64201e2adbd170d41b8377
Sardorbek <sardorbekmusilman@gmail.com>
2025-01-30 19:56:22 +0500

    last

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,30 +2,49 @@
 
 import express from 'express';
 import Product from '../schemas/Product.js';
-import {upload} from "../utils/multer.js"
+import { upload } from "../utils/multer.js"
 const router = express.Router();
 
 
-router.get('/products',  async (req, res, next) => {
+router.get('/products', async (req, res, next) => {
     try {
-       
-        // many to One
-        const products = await Product.find().populate([{path :"categoryId" , select : 'title -_id description'}]);
+        const { category , s } = req.query || {}
+        console.log(category);
+        let q = {}
+        if (category) {
+            q.categoryId = category;
+        }
+        if (s ) {
+            q.name = { $regex: new RegExp(s, 'i') };
+          }
+        const products = await Product.find(q).populate([{ path: "categoryId", select: 'title -_id description' }]);
         res.status(200).send({ data: products });
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+});
+router.get('/products/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id 
+
+        console.log(id);
+        
+        const product = await Product.findById(id).populate([{ path: "categoryId", select: 'title -_id description' }]);
+        res.status(200).send({ data: product });
     } catch (error) {
         res.status(500).send('Error: ' + error.message);
     }
 });
 
 
-router.post("/products" , upload.array('images') ,  async (req, res, next) => {
+router.post("/products", upload.array('images'), async (req, res, next) => {
     try {
-        const images =  req.files.map((file) => file.filename)
+        const images = req.files.map((file) => file.filename)
         console.log(images);
-        if(images.length === 0) {
-           return  res.status(400).send({ message :"Images required"})
+        if (images.length === 0) {
+            return res.status(400).send({ message: "Images required" })
         }
-        const product =  new Product({...req.body , images});
+        const product = new Product({ ...req.body, images });
         await product.save()
         res.status(201).send({ data: product });
     } catch (error) {
